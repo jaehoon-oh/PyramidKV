b61f13f2565554177dfc4991425b89088003b723
Jaehoon Oh <jh0104.oh@gmail.com>
2024-10-16 12:40:07 +0000

    [fix] dataset and max length for llama-3-8b-instruct

--- run_longbench.py
+++ run_longbench.py
@@ -13,8 +13,6 @@ datasets = ["narrativeqa", "qasper", "multifieldqa_en", "hotpotqa", "2wikimqa", 
             "gov_report", "qmsum", "multi_news", "trec", "triviaqa", "samsum", \
             "passage_count", "passage_retrieval_en", "lcc", "repobench-p"]
 
-datasets = ["narrativeqa", "qasper", "multifieldqa_en"]
-
 dataset2maxlen = {
     "narrativeqa": 128,
     "qasper": 128,
@@ -64,7 +62,7 @@ model2prompt = {
 }
 
 model2maxlen = {
-    "meta-llama-3-8b-instruct": 7950,
+    "meta-llama-3-8b-instruct": 7500,
 }
 
 def set_seed(seed):
